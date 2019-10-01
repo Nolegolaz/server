048fa9b15d0bbb1f001062da5f96d4278d8bd19c
Peretyagin Sergey <nolegolaz@gmail.com>
2019-10-01 17:54:19 +0500

    add checks     add decodeURI     add check for /0     add check for close connection - close fs.readStream

--- sendFile.js
+++ sendFile.js
@@ -1,10 +1,18 @@
 'use strict'
 const fs = require('fs');
 
-function sendFile (req, res) {
-    let fileStream = fs.ReadStream(`public${req.url}`);
+function sendFile (reqUrl, res) {
+    let fileStream = fs.ReadStream(`public${reqUrl}`);
 
+    fileStream.on('error', (err) => {
+        res.statusCode = 404;
+        res.end();
+    })
     fileStream.pipe(res);
+
+    res.on('close', () => {
+        fileStream.destroy();
+    })
 };
 
 module.exports = sendFile;
--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const http = require('http');
 const sendFile = require('./sendFile');
 
 const server = http.createServer((req, res) => {
-    
-    sendFile(req, res);
+    let reqUrl = '';
+    try {
+        reqUrl = decodeURIComponent(req.url);
+    } catch (err) {
+        res.statusCode = 400;
+        res.end();
+    };
+
+    if(~req.url.indexOf('/0')) {
+        res.statusCode = 400;
+        res.end();
+    } else {
+        sendFile(reqUrl, res);
+    };
 }).listen(3000, 'localhost');
