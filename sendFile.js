69e1aa1d48e5e947744956e6de0cca1f976d4d02
Peretyagin Sergey <nolegolaz@gmail.com>
2019-10-01 18:43:29 +0500

    add DEBUG log

--- sendFile.js
+++ sendFile.js
@@ -1,10 +1,10 @@
 'use strict'
 const fs = require('fs');
+const debug = require('debug')('sendFile');
 
 function sendFile (reqUrl, res) {
-    console.log(reqUrl);
     let fileStream;
-    
+
     if(reqUrl === '/') {
         fileStream = fs.ReadStream(`public/index.html`);
     } else {
@@ -12,6 +12,7 @@ function sendFile (reqUrl, res) {
     };
 
     fileStream.on('error', (err) => {
+        debug(`Ошибка чтения файла ${err}`);
         res.statusCode = 404;
         res.end();
     })
@@ -19,6 +20,7 @@ function sendFile (reqUrl, res) {
 
     res.on('close', () => {
         fileStream.destroy();
+        debug('Прерванное соединение');
     })
 };
 
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 'use strict';
 const http = require('http');
 const sendFile = require('./sendFile');
+const debug = require('debug')('server');
 
 const server = http.createServer((req, res) => {
     let reqUrl = '';
+
     try {
         reqUrl = decodeURIComponent(req.url);
     } catch (err) {
         res.statusCode = 400;
         res.end();
+        debug('Проблема с декодированием URL');
     };
 
     if(~req.url.indexOf('/0')) {
         res.statusCode = 400;
         res.end();
+        debug('Попытка пропихнуть /0');
     } else {
         sendFile(reqUrl, res);
     };
 }).listen(3000, 'localhost');
+
+debug('server start');
